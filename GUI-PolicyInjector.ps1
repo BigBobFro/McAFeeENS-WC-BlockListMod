3d3aae1b97607c3cf5acc93e5d30ba0e4dea9bae
BigBobFro <100251561+BigBobFro@users.noreply.github.com>
2023-01-26 23:17:57 -0500

    Working on GUI Window

---
+++ GUI-PolicyInjector.ps1
@@ -0,0 +1,96 @@
+<#
+Title: McAfee ENS WebControl Block/Allow policy injector
+Author: Victor Willingham (https://github.com/BigBobFro)
+Use: The McAfee Orchestrator admin WebGUI is limited by design to a max resubmission per section of 2MB
+    This is insufficient when the total number of exceeds 50-75K
+    This limitation is not imposed on the overall size of the policy file when imported.
+
+Requirements:
+    Powershell 5.1
+    Current Exported WC Policy from the same server to which it will be injected
+    CSV file of additional site to block or allow
+
+Current Version: 2.0 [NOW]
+========================
+Update History
+1.0 - Used through CLI and manual cut and paste
+1.1 - Made additional ability to allow sites
+2.0 - Complete rework of script to read in existing policy and output a new file
+
+#>
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 						# Load .NET Assembly
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 				# Load .NET Assembly
+
+
+# Build GUI Window
+
+    $MainWinObj                 = New-object System.Windows.Forms.Form
+    $MainWinObj.Text            = "McAfee ENS-WC Policy Inject"
+    $MainWinObj.size            = New-Object System.Drawing.Size(550,250)
+    $MainWinObj.KeyPreview      = $true
+    $MainWinObj.StartPosition   = "CenterScreen"
+
+# Keys Intercepted
+    $MainWinObj.add_keydown
+    (   {if($_.keycode -eq "Enter")
+            { #Do Enter Things
+            }   
+        }
+    )
+    $MainWinObj.add_keydown
+    (   {if($_.keycode -eq "Escape")
+            {# Do Escape Things
+            }
+        }
+    )
+
+# Create operational object
+    $InputFileNameLabel             = New-Object System.Windows.Forms.Label
+    $InputFileNameLabel.Location    = New-Object System.Drawing.size(25,25)
+    $InputFileNameLabel.Size        = New-Object System.Drawing.Size(170,20)
+    $InputFileNameLabel.Text        = "Input filename (*.xml)"
+
+    $InputFileNameField             = New-Object System.Windows.Forms.TextBox
+    $InputFileNameField.location    = New-Object System.Drawing.Size(205,25)
+    $InputFileNameField.size        = New-Object System.Drawing.Size(150,20)
+    $InputFileNameField.TabStop     = $true
+    $InputFileNameField.TabIndex    = 1
+
+    $SelectFileButton               = New-Object System.Windows.Forms.Button
+    $SelectFileButton.Location      = New-Object System.Drawing.Size(365,25)
+    $SelectFileButton.Size          = New-Object System.Drawing.Size(140,20)
+    $SelectFileButton.Text          = "Select Input File"
+    $SelectFileButton.TabStop       = $true
+    $SelectFileButton.TabIndex      = 2
+    $SelectFileButton.add_click(    {<#Activate Windows File Dialog#>})
+
+    $OutputFileLabel                = New-Object System.Windows.Forms.Label
+    $OutputFileLabel.Location       = New-Object System.Drawing.size(25,120)
+    $OutputFileLabel.Size           = New-Object System.Drawing.Size(170,40)
+    $OutputFileLabel.Text           = "Output File"
+    
+
+    $KwitButton                     = New-Object System.Windows.Forms.Button
+    $KwitButton.Location            = New-Object System.Drawing.Size(365,175)
+    $KwitButton.Size                = New-Object System.Drawing.Size(140,20)
+    $KwitButton.Text                = "Quit"
+    $KwitButton.TabStop             = $true
+    $KwitButton.TabIndex            = 99
+    $KwitButton.add_click(          {<#CLOSE AND QUIT#>})
+
+# Add Controls to Main window
+    $MainWinObj.Controls.add($InputFileNameLabel)
+    $MainWinObj.Controls.add($InputFileNameField)
+    $MainWinObj.Controls.add($SelectFileButton)
+    $MainWinObj.Controls.add($KwitButton)
+    $MainWinObj.Controls.add($OutputFileLabel)
+
+# Activate Main window
+    $MainWinObj.topmost = $true
+    $MainWinObj.add_shown({$MainWinObj.Activate();$InputFileNameField.focus()})
+    [void] $MainWinObj.ShowDialog()
+
+
+
+
